f0e9b70c26176c285ec5ff39e39b06832739dc0a
Jakub Cisło <cubix651@gmail.com>
2021-01-25 23:55:19 +0100

    Implement other classifiers

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier, export_text, plot_tree
 from sklearn.metrics import confusion_matrix
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
 
 attributes = [
     'attr',
@@ -54,17 +56,25 @@ def main():
     all_classes = df[class_column].values
     (train_inputs, test_inputs, train_classes, test_classes) = train_test_split(all_inputs, all_classes, train_size=0.7, random_state=1)
 
-    dtc = DecisionTreeClassifier()
-    dtc.fit(train_inputs, train_classes)
-    class_names = dtc.classes_
+    def run(classifier):
+        print(20*'=')
+        name = str(classifier)
+        print(name)
+        classifier.fit(train_inputs, train_classes)
+        class_names = classifier.classes_
+        score = classifier.score(test_inputs, test_classes)
+        print(score)
 
-    score = dtc.score(test_inputs, test_classes)
-    print(score)
+        predicted_classes = classifier.predict(test_inputs)
+        matrix = confusion_matrix(test_classes, predicted_classes)
+        mdf = pd.DataFrame(matrix, columns=class_names, index=class_names)
+        print(mdf)
 
-    predicted_classes = dtc.predict(test_inputs)
-    matrix = confusion_matrix(test_classes, predicted_classes)
-    mdf = pd.DataFrame(matrix, columns=class_names, index=class_names)
-    print(mdf)
+        return name, score
+    run(DecisionTreeClassifier())
+    run(GaussianNB())
+    for k in [3, 5, 7]:
+        run(KNeighborsClassifier(n_neighbors=k, metric='euclidean'))
 
 if __name__ == '__main__':
     main()
