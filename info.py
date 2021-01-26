46ffa012de91be33ae4c0bed38428f2141c1a289
Jakub Cisło <cubix651@gmail.com>
2021-01-26 22:57:21 +0100

    Add more plots

--- info.py
+++ info.py
@@ -13,37 +13,60 @@ def describe_database(df):
     ], axis=1)
     info.columns = ['min', 'max', 'mean', 'isnull']
     print(info)
-    info.to_csv('database_description.csv')
+    info.to_csv('results/database_description.csv')
 
-def __plot_pie(values, step, title):
+def __plot_pie_with_step(values, title, step):
+    bins=range(0, 11*step, step)
+    __plot_pie_with_bins(values, title, bins)
+
+def __plot_pie_with_bins(values, title, bins):
+    labels = [f'{a}-{b}' for a, b in zip(bins[:-1], bins[1:])]
+    __plot_pie_advanced(values, title, bins, labels)
+
+def __plot_pie_classes(values, title, classes, labels=None):
+    if labels == None:
+        labels = classes
+    bins = [*classes, classes[-1]+1]
+    __plot_pie_advanced(values, title, bins, labels)
+
+def __plot_pie_with_labels(values, title, labels):
+    classes = range(1, len(labels)+1)
+    __plot_pie_classes(values, title, classes, labels)
+
+def __plot_pie_advanced(values, title, bins, labels):
     plt.figure()
-    hist, edges = np.histogram(values, bins=range(0, 10*step + 1, step))
-    labels = [f'{a}-{b}' for a, b in zip(edges[:-1], edges[1:])]
-    plt.pie(hist, labels=labels)
+    hist, edges = np.histogram(values, bins=bins)
+    plt.pie(hist)
     plt.title(title)
+    plt.legend(labels=labels)
     plt.savefig(f'results/{title}.png')
     plt.close()
 
 def __plot_attributes(df):
     for attribute in cfg.attributes:
         attribute_name = cfg.attributes_long_names[attribute]
-        __plot_pie(
+        __plot_pie_with_step(
             df[cfg.person_preference(attribute)].values,
-            10,
-            'Preferencja osoby - ' + attribute_name)
-        __plot_pie(
+            'Preferencja osoby - ' + attribute_name,
+            10)
+        __plot_pie_classes(
             df[cfg.person_opinion(attribute)].values,
-            1,
-            'Opinia osoby o partnerze - ' + attribute_name)
-        __plot_pie(
+            'Opinia osoby o partnerze - ' + attribute_name,
+            range(11))
+        __plot_pie_with_step(
             df[cfg.person_preference(attribute)].values,
-            10,
-            'Preferencja partnera - ' + attribute_name)
-        __plot_pie(
+            'Preferencja partnera - ' + attribute_name,
+            10)
+        __plot_pie_classes(
             df[cfg.person_opinion(attribute)].values,
-            1,
-            'Opinia partnera o osobie - ' + attribute_name)
+            'Opinia partnera o osobie - ' + attribute_name,
+            range(11))
 
 def plot_features(df):
     __plot_attributes(df)
+    __plot_pie_with_bins(df['age'].values, 'Wiek', [18, 20, 22, 24, 26, 28, 30, 35, 40, 55])
+    __plot_pie_with_labels(df['race'].values, 'Rasa', cfg.race_labels)
+    __plot_pie_with_labels(df['date'].values, 'Częstość randkowania', cfg.frequency_labels)
+    __plot_pie_with_labels(df['go_out'].values, 'Częstość wychodzenia (niekoniecznie na randki)', cfg.frequency_labels)
+    __plot_pie_classes(df['exphappy'].values, 'Ogólne szczęście', range(11))
     
